1b6a785b46a79f837b8c60053a4b3e7350afeed0
mounika <mounikagoudlingala@gmail.com>
2023-05-05 16:08:19 +0530

    responsive design(mobile view) completed

---
+++ model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+let Userdetails = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique:true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique:true,
+    }
+})
+
+module.exports=mongoose.model("Userdetails",Userdetails)
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose=require("mongoose")
+const cors = require("cors")
+const Userdetails=require("./model")
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors({ origin: "*" }))
+
+
+mongoose.connect("mongodb+srv://mounika:vV07o6HOA9WkVB00@cluster0.4qioyol.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log("DBconnected"))
+
+app.post("/adduser", async(req, res) => {
+    const { username, email } = req.body
+    try {
+        const newUser = new Userdetails({ username,email} )
+        newUser.save()
+        return res.json(await Userdetails.find)
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+    
+})
+
+app.listen(3003, (req, res) => {
+    console.log("server running")
+})
